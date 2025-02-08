0c32a8f5dd125833a52dc37a6cd44d8099e6ca85
Lukman Muludin <43158553+lukman754@users.noreply.github.com>
2025-02-08 21:38:45 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/lukman754/rekognisi@main/sispema.js";
+script.type = "text/javascript";
+document.head.appendChild(script);
