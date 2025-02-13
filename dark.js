a741a2dff890b91a2ae8025b56e8de8514d2fbfc
Lukman Muludin <43158553+lukman754@users.noreply.github.com>
2025-02-13 22:49:29 +0700

    Update dark.js

--- dark.js
+++ dark.js
@@ -1,11 +1,24 @@
 let darkMode = document.createElement("style");
 darkMode.innerText = `
+  html, body {
+    height: 100%;
+    color: #e0e0e0;
+  }
+
+  * {
+    color: black;
+  }
+    
+  /* Membalik warna halaman kecuali gambar & video */
   html {
     filter: invert(1) hue-rotate(180deg);
-    background: black;
   }
-  img, video {
-    filter: invert(1) hue-rotate(180deg);
+
+  /* Mengembalikan warna asli gambar & video */
+  img, video, iframe {
+    filter: invert(1) hue-rotate(180deg) !important;
   }
+
+
 `;
 document.head.appendChild(darkMode);
